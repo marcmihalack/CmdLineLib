56140b0409d2ace8af94b5f676e4d4b2605435b7
Marcin Michalak <marcin.michalak@outlook.com>
2017-01-17 00:21:48 -0800

    Adding links and whatnots.  Fixing build stuff.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,13 @@
 @echo off
+echo Deleting Release stuff
+for /d %%F in (_build\Release*) do del /s /q "%%F"
+
+echo Building stuff
 msbuild CmdLineLib\CmdLineLib.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5.2
 msbuild CmdLineLib\CmdLineLib.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.6.1 /p:CreatePackage=true /p:PublishDir=..\..\..\nuget\nupkgsrc
 
+echo Deleting other stuff
 del ..\..\nuget\nupkg\cmdlinelib\* /s /f /q
 
+echo Packaging stuff
 ..\nuget.exe init ..\..\nuget\nupkgsrc ..\..\nuget\nupkg
