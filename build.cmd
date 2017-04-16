cdac621d443567c3c5c8ce8fb91d56ebe3613f8e
Marcin Michalak <marcin.michalak@outlook.com>
2017-04-16 14:27:35 -0700

    Fixed build.cmd

--- build.cmd
+++ build.cmd
@@ -1,13 +1,32 @@
 @echo off
 echo Deleting Release stuff
-for /d %%F in (_build\Release*) do del /s /q "%%F"
-
+for /d %%F in (%~dp0_build\Release*) do del /s /q "%%F"
+echo Deleting NuGet stuff
+del %~dp0..\..\nuget\nupkgsrc\CmdLineLib* /s /f /q
+::GOTO EOF
 echo Building stuff
-msbuild CmdLineLib\CmdLineLib.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5.2
-msbuild CmdLineLib\CmdLineLib.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.6.1 /p:CreatePackage=true /p:PublishDir=..\..\..\nuget\nupkgsrc
+::msbuild CmdLineLib\CmdLineLib.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5.2
+::msbuild CmdLineLib\CmdLineLib.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.6.1 /p:CreatePackage=true /p:PublishDir=%~dp0..\..\..\nupkgsrc
 
-echo Deleting other stuff
-del ..\..\nuget\nupkg\cmdlinelib\* /s /f /q
+msbuild CmdLineLib.Tests\CmdLineLib.Tests.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5.2
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+msbuild CmdLineLib.Tests\CmdLineLib.Tests.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.6.1 /p:CreatePackage=true /p:PublishDir=%~dp0..\..\nuget\nupkgsrc
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+mstest.exe /testcontainer:%~dp0_build\Release.v4.5.2\test\CmdLineLib.Tests\CmdLineLib.Tests.dll
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+mstest.exe /testcontainer:%~dp0_build\Release.v4.6.1\test\CmdLineLib.Tests\CmdLineLib.Tests.dll
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+echo NuGet source packages:
+dir %~dp0..\..\nuget\nupkgsrc
+
+echo Deleting NuGet source stuff
+del %~dp0..\..\nuget\nupkg\cmdlinelib\* /s /f /q
 
 echo Packaging stuff
-..\nuget.exe init ..\..\nuget\nupkgsrc ..\..\nuget\nupkg
+%~dp0..\nuget.exe init %~dp0..\..\nuget\nupkgsrc %~dp0..\..\nuget\nupkg
+
+GOTO EOF
+:ERROR
+echo Failed!!!
+:EOF
